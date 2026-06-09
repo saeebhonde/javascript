b44f60064d4ec675b67daaf56f94469e44bd77bc
saeebhonde <saee.bhonde.cc@gmail.com>
2026-06-09 11:58:14 +0530

    initial commit

---
+++ practice.js
@@ -0,0 +1,19 @@
+// Arithmetic Operations
+let a = 10;
+let b = 5;
+
+console.log("Addition =", a + b);
+console.log("Subtraction =", a - b);
+console.log("Multiplication =", a * b);
+console.log("Division =", a / b);
+
+// Loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Number:", i);
+}
+
+// Array
+let fruits = ["Apple", "Banana", "Mango"];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
